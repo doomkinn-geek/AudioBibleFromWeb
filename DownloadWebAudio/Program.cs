f47830c354b4359663811f52d2e877d8853ef0bf
doomkinn-geek <doomkinn@yandex.ru>
2023-07-18 14:57:39 +0300

    Добавьте файлы проекта.

---
+++ DownloadWebAudio/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Newtonsoft.Json.Linq;
+
+namespace DownloadWebAudio
+{
+    internal class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            var url = "https://knigavuhe.org/book/biblija-novyjj-zavet-sinodalnyjj-perevod/"; // URL страницы
+            var html = await client.GetStringAsync(url); // Получаем HTML-код страницы
+
+            var regex = new Regex(@"new BookPlayer\(\d+, (\[.*?\])"); // Регулярное выражение для поиска JSON-данных
+            var match = regex.Match(html); // Ищем JSON-данные в HTML-коде
+
+            if (match.Success)
+            {
+                var jsonData = match.Groups[1].Value; // Извлекаем JSON-данные
+                var jsonArray = JArray.Parse(jsonData); // Преобразуем JSON-данные в JArray
+
+                foreach (var jsonObject in jsonArray)
+                {
+                    var audioUrl = (string)jsonObject["url"]; // Получаем URL аудиофайла
+                    if (audioUrl != null)
+                    {
+                        Console.WriteLine($"Found audio file: {audioUrl}");
+                        await DownloadAudioFile(audioUrl); // Скачиваем аудиофайл
+                    }
+                }
+            }
+        }
+        static async Task DownloadAudioFile(string url)
+        {
+            var fileName = Path.GetFileName(new Uri(url).LocalPath); // Получаем имя файла из URL
+            var bytes = await client.GetByteArrayAsync(url); // Скачиваем файл
+            await File.WriteAllBytesAsync(fileName, bytes); // Сохраняем файл на диск
+            Console.WriteLine($"Downloaded: {fileName}");
+        }
+    }
+}
---
+++ SortBibleFiles/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace SortBibleFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Список книг Нового Завета
+            string[] books = new string[]
+            {
+            "Евангелие от Матфея",
+            "Евангелие от Марка",
+            "Евангелие от Луки",
+            "Евангелие от Иоанна",
+            "Деяния святых апостолов",
+            "Послание Иакова",
+            "1-е послание Петра",
+            "2-е послание Петра",
+            "1-е послание Иоанна",
+            "2-е послание Иоанна",
+            "3-е послание Иоанна",
+            "Послание Иуды",
+            "Послание к Римлянам",
+            "1-е послание к Коринфянам",
+            "2-е послание к Коринфянам",
+            "Послание к Галатам",
+            "Послание к Ефесянам",
+            "Послание к Филиппийцам",
+            "Послание к Колоссянам",
+            "1-е послание к Фессалоникийцам",
+            "2-е послание к Фессалоникийцам",
+            "1-е послание к Тимофею",
+            "2-е послание к Тимофею",
+            "Послание к Титу",
+            "Послание к Филимону",
+            "Послание к Евреям",
+            "Откровение Иоанна Богослова"
+            };
+
+            // Путь к папке с исходными файлами
+            string sourceDirectory = @"d:\\Temp\\audio_new_tastament\\";
+
+            foreach (string file in Directory.GetFiles(sourceDirectory, "*.mp3"))
+            {
+                string fileName = Path.GetFileNameWithoutExtension(file);
+                string[] parts = fileName.Split('-');
+
+                if (parts.Length != 2)
+                {
+                    Console.WriteLine($"Файл {fileName} имеет неправильный формат имени.");
+                    continue;
+                }
+
+                if (!int.TryParse(parts[1], out int bookIndex) || bookIndex < 0 || bookIndex >= books.Length)
+                {
+                    Console.WriteLine($"Файл {fileName} имеет неправильный номер книги.");
+                    continue;
+                }
+
+                string bookName = books[bookIndex];
+                string targetDirectory = Path.Combine(sourceDirectory, bookName);
+
+                if (!Directory.Exists(targetDirectory))
+                {
+                    Directory.CreateDirectory(targetDirectory);
+                }
+
+                string targetFile = Path.Combine(targetDirectory, Path.GetFileName(file));
+
+                File.Move(file, targetFile);
+
+                // Изменение метаданных MP3 файла
+                var mp3 = TagLib.File.Create(targetFile);
+                mp3.Tag.Performers = new string[] { bookName };
+                mp3.Save();
+            }
+        }
+    }
+}
